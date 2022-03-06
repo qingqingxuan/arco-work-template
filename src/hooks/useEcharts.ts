443c401f464076854fef07b153c6294329489c6b
jxl <738179011@qq.com>
2022-03-06 21:00:37 +0800

    init

--- src/hooks/useEcharts.ts
+++
@@ -1,8 +0,0 @@
-import echarts from '../utils/echarts'
-export default function useEcharts(dom: HTMLElement, theme?: string) {
-  let instance = echarts.getInstanceByDom(dom)
-  if (!instance) {
-    instance = echarts.init(dom, theme, { renderer: 'svg' })
-  }
-  return instance
-}
